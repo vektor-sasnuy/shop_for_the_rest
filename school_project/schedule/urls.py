795dab9283e3a458dcf3aedbe31b23413bf4c888
vektor-sasnuy <aleksandrsolomay@gmail.com>
2025-09-27 11:22:05 +0300

    global apdate project

--- school_project/schedule/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from schedule.views import ProductListView, HomePageView, ProductDetailView, AddToCartView
-
-urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('products/', ProductListView.as_view(), name='product-list'),
-    path('products/<int:product_id>/', ProductDetailView.as_view(), name='product-detail'),
-    path('products/<int:product_id>/add-to-cart/', AddToCartView.as_view(), name='add_to_cart'),
-]
--- school_project/school_project/settings.py
+++ school_project/school_project/settings.py
@@ -63,5 +63,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
